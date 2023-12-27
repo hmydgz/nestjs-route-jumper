39ec8319069550f9e6887a6302bee1a901c29257
hmydgz <920136080@qq.com>
2023-12-28 01:37:44 +0800

    ✨ feat: app 新增 ast 解析，判断文件中是否有 nest app 入口

--- src/project/app.ts
+++ src/project/app.ts
@@ -1,14 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { Project } from './index'
-
-// type DependencyTree = any
+import * as ts from 'typescript'
 
 /**
  * Nestjs App
  */
 export class NestjsApp {
-  mainFile?: string
+  mainFile!: string
   entryFile: string;
   sourceRoot: string;
   project: Project;
@@ -24,7 +23,26 @@ export class NestjsApp {
     fs.readdirSync(sourceRoot, { withFileTypes: true }).forEach(v => {
       if (v.name.startsWith(entryFile)) {
         this.mainFile = path.join(sourceRoot, v.name);
+        this.parseMain()
       }
     })
+  }
+
+  parseMain() {
+    const ast = ts.createSourceFile(
+      this.mainFile,
+      fs.readFileSync(this.mainFile).toString(),
+      ts.ScriptTarget.Latest,
+    )
+
+    // ts.forEachChild(ast, (node) => {
+    //   console.log(`Node: ${ts.SyntaxKind[node.kind]}`, node);
+    // });
+
+    const identifiers = (ast as any).identifiers as Map<string, string>
+
+    if (identifiers.has('NestFactory') && identifiers.has('create')) { // 可能存在 NestFactory.create，需要检查
+      console.log(this.mainFile, identifiers)
+    }
   }
 }
