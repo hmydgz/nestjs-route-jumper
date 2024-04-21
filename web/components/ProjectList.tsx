7da8fb4bc690a48b78fc5c61792f9724a494ab45
hmydgz <920136080@qq.com>
2024-04-21 18:48:28 +0800

    ✨ feat: 新增总数和查询的数量展示

--- web/components/ProjectList.tsx
+++ web/components/ProjectList.tsx
@@ -2,6 +2,11 @@ import { useStore } from "../store"
 import AppList from "./AppList"
 import { CodiconFolder } from "./Icon"
 
+
+function getCount(apps: any[]) {
+  return apps.reduce((acc, cur) => acc + cur.mappings.length, 0)
+}
+
 const ProjectList: React.FC = () => {
   const [store] = useStore()
   function getDir(path: string) {
@@ -14,8 +19,8 @@ const ProjectList: React.FC = () => {
     {store.renderProjects.map(v => v.apps.length ? <div>
       { v.dirPath ? <div className="py-1 flex items-center">
         <CodiconFolder className="text-base" />
+        <span className="pl-1">({store.searchStr ? getCount(v.apps) + '/' : ''}{ getCount(store.projects.find(_v => _v.dirPath === v.dirPath)?.apps ?? []) })</span>
         <span className="pl-1">{ getDir(v.dirPath) }</span>
-        <span></span>
       </div> : null }
       <div className={store.renderProjects.length > 1 ? 'pl-1' : ''}>
         <AppList apps={v.apps} />
